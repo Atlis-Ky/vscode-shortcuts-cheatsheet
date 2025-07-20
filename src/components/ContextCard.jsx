7364e8db96470406b02fb69b88d6c68ce52792f2
Ky <kylesmith97@outlook.com>
2025-07-20 12:51:34 +0100

    fonts and css imported + updated

--- src/components/ContextCard.jsx
+++ src/components/ContextCard.jsx
@@ -3,12 +3,12 @@ import "../styles/ContextCard.css";
 
 const ContextCard = () => (
   <div className="context-card">
-    <h1 className="context-card-heading">Find The Right Shortcut For You</h1>
+    <h1 className="context-card-heading">Speed up your programming</h1>
     <p className="context-card-text">
-      This page shows you some of the most commonly used keyboard shortcuts for
+      This page shows you some of the most useful keyboard shortcuts for
       Visual Studio Code that will help speed up your programming adventures.
       I've split them up into categories and given each its own colour palette
-      to help associative learners like me!
+      to help associative learners like me! Feel free to click on any of the preset filters or use the search bar to find a specific shortcut that you need.
     </p>
   </div>
 );
